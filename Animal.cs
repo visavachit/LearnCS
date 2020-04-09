1df9d067844e131112b0cfe25a284f090507c6fe
Visavachit Suriyachant <suriyachant.v@outlook.co.th>
2020-04-09 18:11:03 +0700

    Add an Animal class and Cat class, try to use if function in Animal class (if an animal have legs will use Walk method or don't have legs use Swim method.

---
+++ Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnCS
+{
+    class Animal
+    {
+        string breed;
+        int age;
+        bool leg;
+        /*public void Legs()
+        {
+            if (leg == true)
+                return;        }*/
+        public void Eat()
+        {
+
+        }
+        public void Walk()
+        {
+            
+        }
+        public void Sleep()
+        {
+
+        }
+        public void Swim()
+        {
+
+        }
+
+        
+    }
+}       
